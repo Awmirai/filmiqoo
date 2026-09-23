1f76b60f1b03e1973339daf232f090c1c6fb464e
Awmirai <140288651+Awmirai@users.noreply.github.com>
2026-09-23 07:08:44 +0200

    Fix setup screen shape import

--- app/src/main/java/com/filmiqoo/app/MainActivity.kt
+++ app/src/main/java/com/filmiqoo/app/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
