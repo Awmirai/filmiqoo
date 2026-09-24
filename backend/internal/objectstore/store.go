fd9b6bd8f09328d2990e817d8cfef8dd417129ba
Awmirai <140288651+Awmirai@users.noreply.github.com>
2026-09-24 23:30:44 +0200

    Make object storage readiness retryable

--- backend/internal/objectstore/store.go
+++ backend/internal/objectstore/store.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/url"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/minio/minio-go/v7"
@@ -16,8 +15,6 @@ type Store struct {
 	internal *minio.Client
 	signer *minio.Client
 	bucket string
-	once sync.Once
-	ensureErr error
 }
 
 func New(internalEndpoint, publicEndpoint, accessKey, secretKey, bucket string) (*Store,error) {
@@ -42,14 +39,20 @@ func newClient(endpoint,accessKey,secretKey string) (*minio.Client,error) {
 }
 
 func (s *Store) ensure(ctx context.Context) error {
-	s.once.Do(func(){
-		exists,err:=s.internal.BucketExists(ctx,s.bucket)
-		if err!=nil { s.ensureErr=err; return }
-		if !exists {
-			s.ensureErr=s.internal.MakeBucket(ctx,s.bucket,minio.MakeBucketOptions{})
-		}
-	})
-	return s.ensureErr
+	exists,err:=s.internal.BucketExists(ctx,s.bucket)
+	if err!=nil { return err }
+	if exists { return nil }
+	if err:=s.internal.MakeBucket(ctx,s.bucket,minio.MakeBucketOptions{}); err!=nil {
+		existsAgain,checkErr:=s.internal.BucketExists(ctx,s.bucket)
+		if checkErr==nil && existsAgain { return nil }
+		return err
+	}
+	return nil
+}
+
+func (s *Store) Health(ctx context.Context) error {
+	if s==nil { return fmt.Errorf("object store is nil") }
+	return s.ensure(ctx)
 }
 
 func (s *Store) PresignPut(ctx context.Context,key string,expiry time.Duration) (*url.URL,error) {
