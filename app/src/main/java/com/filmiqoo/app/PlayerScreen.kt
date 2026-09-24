52fa1947d3e2aff430a71daa4b235e34768ed710
Awmirai <140288651+Awmirai@users.noreply.github.com>
2026-09-24 11:11:07 +0200

    Fix player episode queue image ContentScale import

--- app/src/main/java/com/filmiqoo/app/PlayerScreen.kt
+++ app/src/main/java/com/filmiqoo/app/PlayerScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
