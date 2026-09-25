df5465518cb745d10c34c1777415258ae38583d3
Awmirai <140288651+Awmirai@users.noreply.github.com>
2026-09-25 12:44:47 +0200

    Import click handling for custom bottom navigation

--- app/src/main/java/com/filmiqoo/app/MainActivity.kt
+++ app/src/main/java/com/filmiqoo/app/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
